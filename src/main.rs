363d268635e1c3e2c4fcdd04fbc301f1a2543010
123-harvecraft <tech.omnicraft@gmail.com>
2026-09-12 18:52:33 +0700

    update

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::sync::Arc;
+
+use anyhow::Context;
+use clap::Parser;
+use dotenvy::dotenv;
+
+use tracing::info;
+
+use rest_api::{AppConfig, ApplicationServer, Database, Logger};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenv().ok();
+    let config = Arc::new(AppConfig::parse());
+
+    let _guard = Logger::init(config.cargo_env);
+
+    info!("environment loaded and configuration parsed, initializing Postgres connection and running migrations...");
+    let db = Database::connect(&config.database_url, config.run_migrations)
+        .await
+        .expect("could not initialize the database connection pool");
+
+    ApplicationServer::serve(config, db)
+        .await
+        .context("could not initialize application routes")?;
+
+    Ok(())
+}
